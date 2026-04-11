09f7369f7b4a97d1f2ac86c1e12b96112700cb72
krazytop <clement.tell44@gmail.com>
2026-04-11 10:32:54 +0200

    datavalues

--- src/main/java/com/krazytop/leagueoflegends/nomenclature/AugmentNomenclature.java
+++ src/main/java/com/krazytop/leagueoflegends/nomenclature/AugmentNomenclature.java
@@ -3,11 +3,12 @@ package com.krazytop.leagueoflegends.nomenclature;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.List;
 import java.util.Map;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AugmentNomenclature extends Nomenclature {
 
-    private Map<String, Float> dataValues;
+    private Map<String, List<Float>> dataValues;
 }
